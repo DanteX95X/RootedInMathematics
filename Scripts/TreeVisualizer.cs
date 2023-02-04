7ed56ed126632cd0257de8c63c0be0fecfaafd4e
Dante <daniellewinski95@gmail.com>
2023-02-04 20:18:04 +0100

    PlayerCharacter and camera rotate to face proper direction.

--- Scripts/TreeVisualizer.cs
+++ Scripts/TreeVisualizer.cs
@@ -1,18 +1,19 @@
+using System.Collections.Generic;
 using Godot;
 using System.Globalization;
 using RootedInMathematics.Scripts;
 
-public class TreeVisualizer : Node
+public class TreeVisualizer : Node2D
 {
 	[Export]
 	private PackedScene treeNodeScene = null;
 
 	private GameSystems gameSystems;
-
 	private Node2D playerCharacter;
-	
+	private Camera2D camera;
 	private Vector2 cachedPosition = Vector2.Zero;
-	
+	private Dictionary<TreeNode, Node2D> modelViewMapping = new Dictionary<TreeNode, Node2D>();
+
 	public override void _Ready()
 	{
 		GD.Print(GetPath());
@@ -20,21 +21,54 @@ public class TreeVisualizer : Node
 		gameSystems.GameLogic.OnMoveToNode += OnMovedToNode;
 
 		playerCharacter = GetNode<Node2D>("PlayerCharacter");
+		camera = playerCharacter.GetNode<Camera2D>("Camera");
 	}
 
-	private void OnMovedToNode(TreeNode currentNode, TreeNode previousNode)
+	private void OnMovedToNode(TreeNode destination, TreeNode source)
 	{
-		var nodeView = (Node2D)treeNodeScene.Instance();
-		AddChild(nodeView);
-		nodeView.Position = cachedPosition;
-		cachedPosition += new Vector2(0, 200);
-		var textNode = nodeView.GetNode<Label>("Container/Text");
-		if (currentNode.NodeValue != null)
+		Vector2 lookingPosition = Vector2.Up;
+		if (!modelViewMapping.TryGetValue(destination, out Node2D destinationView))
 		{
-			textNode.Text = currentNode.NodeValue.value.ToString(CultureInfo.CurrentCulture);
+			destinationView = (Node2D) treeNodeScene.Instance();
+			AddChild(destinationView);
+			var textNode = destinationView.GetNode<Label>("Container/Text");
+			if (destination.NodeValue != null)
+			{
+				textNode.Text = destination.NodeValue.value.ToString(CultureInfo.CurrentCulture);
+			}
+			
+			if (source == null || !modelViewMapping.TryGetValue(source, out Node2D sourceView))
+			{
+				destinationView.Position = Vector2.Zero;
+			}
+			else
+			{
+				sourceView.Visible = false;
+				int childIndex = source.Children.FindIndex(data => data.node == destination);
+				
+				Vector2 displacement = Vector2.Down * 200;
+				if (source.Parent != null && modelViewMapping.TryGetValue(source.Parent, out Node2D sourceParentView))
+				{
+					displacement = sourceView.Position - sourceParentView.Position;
+				}
+
+				Vector2 currentDisplacement = displacement.Rotated(-1 * (childIndex - 1));
+				destinationView.Position = sourceView.Position + currentDisplacement;
+
+				lookingPosition = sourceView.Position;
+			}
+			
+			modelViewMapping.Add(destination, destinationView);
+			// destinationView.LookAt(lookingPosition);
+			// destinationView.Rotate(1.5708f);
 		}
 
-		playerCharacter.Position = nodeView.Position;
+		destinationView.Visible = true;
+		playerCharacter.Position = destinationView.Position;
+		playerCharacter.LookAt(lookingPosition);
+		destinationView.Rotation = playerCharacter.Rotation;
+		destinationView.Rotate(1.5708f);
+		
 	}
 
 	public override void _Process(float delta)
